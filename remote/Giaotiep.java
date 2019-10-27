797a8d28902275055176225cb592209feac9f160
thuydemons97 <thuydemons@gmail.com>
2019-10-27 15:29:06 +0700

    update remote

--- remote/Giaotiep.java
+++ remote/Giaotiep.java
@@ -4,7 +4,19 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Giaotiep extends Remote {
-	public void keypress(int i) throws RemoteException;
+	public void keypress(int i) throws RemoteException ;
 
 	public void keyReleased(int i) throws RemoteException;
+
+	public void type(int i) throws RemoteException;
+
+	public void move(int x, int y) throws RemoteException;
+
+	public void wheel(int i) throws RemoteException;
+
+	public int[] size() throws RemoteException;
+
+	public void mouseclick(int a, int count) throws RemoteException;
+
+	public void mouseReleased(int a, int count) throws RemoteException;
 }
