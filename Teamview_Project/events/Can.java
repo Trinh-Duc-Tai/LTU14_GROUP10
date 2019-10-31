3e0176db8fcf77880954acda1dafbfa5e3d38fc1
Trinh Duc Tai <trinhductai6869@gmail.com>
2019-10-31 16:12:08 +0700

    update Canvas demo tu Thuy

---
+++ Teamview_Project/events/Can.java
@@ -0,0 +1,21 @@
+package events ;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+public class Can extends Canvas {
+	
+	private static final long serialVersionUID = 89981L;
+	BufferedImage i;
+
+	public Can(BufferedImage in) {
+		// TODO Auto-generated constructor stub
+		i = in;
+	}
+
+	public void paint(Graphics g) {
+		super.paint(g);
+		g.drawImage(i, 0, 0, null);
+	}
+}
