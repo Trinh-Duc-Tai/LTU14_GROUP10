d957ad3e361d3e2c74b2c9c1016838c279adaa2e
Trinh Duc Tai <trinhductai6869@gmail.com>
2019-11-01 20:53:38 +0700

    update Mouse Event demo

--- Teamview_Project/events/MouseEventDemo.java
+++ Teamview_Project/events/MouseEventDemo.java
@@ -94,5 +94,26 @@ public class MouseEventDemo extends JPanel
         textArea.setCaretPosition(textArea.getDocument().getLength());
     }
     
+    public void mousePressed(MouseEvent e) {
+        eventOutput("Mouse pressed (# of clicks: "
+                + e.getClickCount() + ")", e);
+    }
     
+    public void mouseReleased(MouseEvent e) {
+        eventOutput("Mouse released (# of clicks: "
+                + e.getClickCount() + ")", e);
+    }
+    
+    public void mouseEntered(MouseEvent e) {
+        eventOutput("Mouse entered", e);
+    }
+    
+    public void mouseExited(MouseEvent e) {
+        eventOutput("Mouse exited", e);
+    }
+    
+    public void mouseClicked(MouseEvent e) {
+        eventOutput("Mouse clicked (# of clicks: "
+                + e.getClickCount() + ")", e);
+    }
 }
