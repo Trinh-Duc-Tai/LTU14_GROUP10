e14d401479cdf1b926f58336d36257275a4b6510
MaiThiThuHuyen <MaiThiThuHuyen96@users.noreply.github.com>
2019-11-02 21:57:12 +0700

    code mouse capture

---
+++ Teamview_Project/teamview/act/MouseCapture.java
@@ -0,0 +1,41 @@
+package teamview.act;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import remote.Giaotiep;
+
+public class MouseCapture implements MouseListener {
+	Giaotiep gt ;
+	public MouseCapture(Giaotiep s)
+	{
+		gt=s; 
+	}
+	public void mouseClicked(MouseEvent e) {
+	try {
+		gt.mouseReleased(e.getButton(), e.getClickCount());
+	} catch (Exception e2) {
+		e2.printStackTrace();	
+	}
+	
+		
+	}
+	public void mouseReleased(MouseEvent e) {
+		try {
+			gt.mouseReleased(e.getButton(), e.getClickCount());
+		} catch (Exception e2) {
+			e2.printStackTrace();	
+		}
+		
+	}
+	
+	public void mouseEntered(MouseEvent e) {
+		
+		//no thinh to do
+		
+	}
+	public void mouseExited(MouseEvent e) {
+		// do no thing
+		
+	}
+}
