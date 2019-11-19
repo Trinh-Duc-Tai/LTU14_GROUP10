a9bcc66ae4ed85a3a754550e7ad97794e7732a61
Trinh Duc Tai <trinhductai6869@gmail.com>
2019-11-20 00:28:23 +0700

    update events show

--- Teamview_Project/events/show.java
+++ Teamview_Project/events/show.java
@@ -5,7 +5,14 @@ import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
 
 public class show {
-	
+	public static void pr(BufferedImage in) {
+		JFrame f = new JFrame("test");
+		f.setSize(in.getWidth() + 20, in.getHeight() + 20);
+		f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		f.setVisible(true);
+
+		f.add(new Can(in));
+	}
 
 	public static void pr(BufferedImage in, String d) {
 		JFrame f = new JFrame(d);
